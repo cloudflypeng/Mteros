e2b31b8a6d4f164d898f1ec8e3d5ca3b4e4f81f2
cloudflypeng <cloudflypeng@gmail.com>
2024-12-30 21:25:30 +0800

    fix: 删除无用的路由

--- src/app/api/login/route.ts
+++
@@ -1,7 +0,0 @@
-// login
-
-// https://passport.bilibili.com/x/passport-login/web/sms/send
-// export async function GET(request: NextRequest) {
-//   const { searchParams } = new URL(request.url)
-//   const code = searchParams.get('code')
-// }
