03375be0205d0e2041dc4225bd6e30965519b2d0
cloudflypeng <cloudflypeng@gmail.com>
2024-12-31 23:07:06 +0800

    feat: 删除首页

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li>There is Mteros</li>
-          {/* 跳转到搜索 */}
-          <li><a href="/search">搜索</a></li>
-          <li><a href="/collection">收藏</a></li>
-          <li><a href="/login">登录</a></li>
-        </ol>
-      </main>
+      未知错误,请退出
     </div>
   );
 }
